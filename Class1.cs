c678cffd3ed512c121e0939c69f0a9c32fde7ba8
Naydenova <lnaiti@mail.ru>
2026-02-04 12:33:01 +0500

    nenene

--- Class1.cs
+++ Class1.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    internal class Class1
+    public static class Calculator
     {
+        // Сложение двух чисел
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        // Вычитание двух чисел
+        public static double Substract(double a, double b)
+        {
+            return a - b;
+        }
+        // Умножение двух чисел
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        // Деление двух чисел
+        public static double Divide(double a, double b)
+        {
+            return a / b;
+        }
     }
+
 }
